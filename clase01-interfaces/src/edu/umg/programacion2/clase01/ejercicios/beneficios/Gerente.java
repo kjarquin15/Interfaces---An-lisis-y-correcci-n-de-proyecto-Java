046d50cb43830e6309a75b45367280f2f02413ee
KAREN LIZZETH JARQUÍN SALGUERO <kjarquins@miumg.edu.gt>
2026-07-24 22:40:48 -0600

    fix clase gerente

--- clase01-interfaces/src/edu/umg/programacion2/clase01/ejercicios/beneficios/Gerente.java
+++ clase01-interfaces/src/edu/umg/programacion2/clase01/ejercicios/beneficios/Gerente.java
@@ -22,8 +22,7 @@ public class Gerente implements Bonificable {
 
 	@Override
 	public double calcularBono(double salarioBase) {
-		// TODO: reemplazar esta línea por la fórmula correcta.
-		throw new UnsupportedOperationException("TODO: completar calcularBono() en Gerente");
+	    return 500 + (100 * aniosServicio);
 	}
 
 	public String getNombre() {
